c8fae899bd6005fdde1e90a390d0bc6cc5ecefb7
nadralia <nadralia@gmail.com>
2022-05-30 16:43:26 +0200

    insert items to editcount table

--- index.php
+++ index.php
@@ -32,6 +32,38 @@ if ($link->errno) {
 }
  
 
+// Attempt insert query execution
+$sql = "INSERT INTO editcount (username, title, count) VALUES
+            ('UserA', 'Apples', 1),
+            ('UserA', 'Bananas', 1),
+            ('UserA', 'Durian', 1),
+            ('UserB', 'Bananas', 1),
+            ('UserB', 'Carrots', 1),
+            ('UserB', 'Durian', 1),
+            ('UserC', 'Apples', 1),
+            ('UserC', 'Bananas', 1),
+            ('UserC', 'Carrots', 1),
+            ('UserC', 'Durian', 1),
+            ('UserD', 'Durian', 1),
+            ('UserE', 'Apples', 1),
+            ('UserE', 'Bananas', 1),
+            ('UserE', 'Carrots', 1),
+            ('UserE', 'Durian', 1),
+            ('UserF', 'Apples', 1),
+            ('UserF', 'Bananas', 1),
+            ('UserF', 'Carrots', 1),
+            ('UserF', 'Durian', 1),
+            ('UserG', 'Apples', 1),
+            ('UserH', 'Bananas', 1),
+            ('UserH', 'Carrots', 1),
+            ('UserH', 'Durian', 1)";
+
+if(mysqli_query($link, $sql)){
+    echo "Records added successfully.";
+} else{
+    echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
+}
+
 
 // Close connection
 mysqli_close($link);
